bb96f82657e1f09f53462d2bc4797f6eecdd1b9b
Silva <vagner.antonio.silva@accenture.com>
2020-07-28 01:10:12 -0300

    add lambda pytorch function

---
+++ lambda_pytorch/lambda_function.py
@@ -0,0 +1,20 @@
+import boto3
+
+def lambda_handler(event, context):
+
+    # The SageMaker runtime is what allows us to invoke the endpoint that we've created.
+    runtime = boto3.Session().client('sagemaker-runtime')
+
+    # Now we use the SageMaker runtime to invoke our endpoint, sending the review we were given
+    response = runtime.invoke_endpoint(EndpointName = 'sagemaker-pytorch-2020-07-27-16-13-48-062',    # The name of the endpoint we created
+                                       ContentType = 'text/plain',                 # The data format that is expected
+                                       Body = event['body'])                       # The actual review
+
+    # The response is an HTTP response whose body contains the result of our inference
+    result = response['Body'].read().decode('utf-8')
+
+    return {
+        'statusCode' : 200,
+        'headers' : { 'Content-Type' : 'text/plain', 'Access-Control-Allow-Origin' : '*' },
+        'body' : result
+    }
